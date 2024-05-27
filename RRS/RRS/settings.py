b78525f76cbb44b8cc5e13f0b6301b91a6953493
Marten Schulz <ms@marten-schulz.de>
2024-05-27 13:09:09 +0200

    Models hinzugefügt

--- RRS/RRS/settings.py
+++ RRS/RRS/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'ReservationManagement.apps.ReservationmanagementConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
